6714cca223019bdd1b7fa40c0a27320b8f940fe4
Byron4j <1552546443@qq.com>
2019-05-06 18:16:14 +0800

    :beer: Spring-Eureka

---
+++ eureka-cli-1/src/test/java/org/byron4j/eurekacli1/EurekaCli1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.byron4j.eurekacli1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaCli1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ src/main/java/org/byron4j/springcloud/SpringcloudApplication.java
@@ -0,0 +1,13 @@
+package org.byron4j.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudApplication.class, args);
+    }
+
+}
